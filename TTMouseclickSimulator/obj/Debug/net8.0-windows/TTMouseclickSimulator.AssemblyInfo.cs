27b2b492da02e20a569947eeb10de63bd563f63a
AdamNur422 <143763008+AdamNur422@users.noreply.github.com>
2024-04-01 20:49:05 -0400

    a

--- TTMouseclickSimulator/obj/Debug/net8.0-windows/TTMouseclickSimulator.AssemblyInfo.cs
+++ TTMouseclickSimulator/obj/Debug/net8.0-windows/TTMouseclickSimulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TTMouseclickSimulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5650d97f37c73ff888e49862c6d15160cc3fe0a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd634a4a478bd7d379b54fac57504daab78169d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TTMouseclickSimulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TTMouseclickSimulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
